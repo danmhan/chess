1c1fa76c3117d9f2b498c5444e655408d76d8f8d
Daniel Han <55417737+danmhan@users.noreply.github.com>
2021-08-19 17:17:46 -0500

    added isWhite and isBlack functions to Color class. Added legal pawn moves except en passant

--- src/model/Game/board/BoardUtils.java
+++ src/model/Game/board/BoardUtils.java
@@ -2,11 +2,15 @@ package model.Game.board;
 
 public class BoardUtils {
 
-//    public static final boolean[] FIRST_COLUMN = {0, 8, 16, 24, 32, 40, 48, 56};
+    // Columns go in order of 1-a, 2-b
+    // Rows go from black to white.
     public static final boolean[] FIRST_COLUMN = initColumn(0);
     public static final boolean[] SECOND_COLUMN = initColumn(1);
     public static final boolean[] SEVENTH_COLUMN = initColumn(6);
     public static final boolean[] EIGHTH_COLUMN = initColumn(7);
+
+    public static final boolean[] SECOND_ROW = initRow(2);
+    public static final boolean[] SEVENTH_ROW = initRow(7);
 
     private BoardUtils() {
         throw new RuntimeException("Cannot instantiate BoardUtils");
@@ -22,6 +26,14 @@ public class BoardUtils {
             columnNum += 8;
         }
         return columnSquares;
+    }
+
+    private static boolean[] initRow(int rowNum) {
+        boolean[] rowSquares = new boolean[64];
+        for (int i = 0; i < 8; i++) {
+            rowSquares[8 * (rowNum - 1) + i] = true;
+        }
+        return rowSquares;
     }
 
 }
--- src/model/pieces/Pawn.java
+++ src/model/pieces/Pawn.java
@@ -1,16 +1,17 @@
 package model.pieces;
 
-import model.Game.Coordinate;
-import model.Game.Move;
-import model.Game.PieceType;
-import model.Game.Player;
+import model.Game.*;
 import model.Game.board.Board;
+import model.Game.board.BoardUtils;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Pawn extends Piece {
 
-    private final static int[] CANDIDATE_MOVE_COORDINATES = {-16, -9, -8, -7, 7, 8, 9, 16};
+    private final static int[] CANDIDATE_MOVE_COORDINATES = {7, 8, 9, 16};
+    private boolean isFirstMove;
 
     /**
      * The constructor for an abstract Chess Piece
@@ -20,6 +21,7 @@ public class Pawn extends Piece {
      */
     public Pawn(Player player, int position) {
         super(player, position);
+        this.isFirstMove = true;
     }
 
     @Override
@@ -29,7 +31,36 @@ public class Pawn extends Piece {
 
     @Override
     public List<Move> calculateLegalMoves(Board board) {
-        return null;
+        final List<Move> legalMoves = new ArrayList<>();
+        for (final int candidateCoordinateOffset : CANDIDATE_MOVE_COORDINATES) {
+            int candidateDestinationPosition = this.position + (this.getColor().getDirection() * candidateCoordinateOffset);
+
+            if (candidateCoordinateOffset == 8 && !board.getSquare(candidateDestinationPosition).isOccupied()) {
+                legalMoves.add(new Move.MajorMove(board, this, candidateDestinationPosition));
+            } else if (candidateCoordinateOffset == 16 && this.isFirstMove &&
+                ((BoardUtils.SEVENTH_ROW[this.position] && this.getColor() == Color.WHITE) ||
+                (BoardUtils.SECOND_ROW[this.position] && this.getColor() == Color.BLACK))) {
+                final int betweenJumpCoordinate = this.position + (this.getColor().getDirection() * 8);
+                if (!board.getSquare(betweenJumpCoordinate).isOccupied() && !board.getSquare(candidateDestinationPosition).isOccupied())
+                    legalMoves.add(new Move.MajorMove(board, this, candidateDestinationPosition));
+            } else if (!isFirstColumnExclusion(this.position, candidateCoordinateOffset, this.getColor()) &&
+                !isEighthColumnExclusion(this.position, candidateCoordinateOffset, this.getColor())) {
+                final Piece pieceOnCandidateSquare = board.getSquare(candidateDestinationPosition).getPiece();
+                if (pieceOnCandidateSquare != null && this.getColor() != pieceOnCandidateSquare.getColor()) {
+                    legalMoves.add(new Move.AttackMove(board, this, candidateDestinationPosition, pieceOnCandidateSquare));
+                }
+            }
+        }
+        return Collections.unmodifiableList(legalMoves);
     }
 
+    private boolean isFirstColumnExclusion(int currentPosition, int coordinateOffset, Color color) {
+        return (coordinateOffset == 7 && BoardUtils.FIRST_COLUMN[currentPosition] && color.isBlack()) ||
+            (coordinateOffset == 9 && BoardUtils.FIRST_COLUMN[currentPosition] && color.isWhite());
+    }
+
+    private boolean isEighthColumnExclusion(int currentPosition, int coordinateOffset, Color color) {
+        return (coordinateOffset == 9 && BoardUtils.EIGHTH_COLUMN[currentPosition] && color.isBlack()) ||
+            (coordinateOffset == 7 && BoardUtils.EIGHTH_COLUMN[currentPosition] && color.isWhite());
+    }
 }
